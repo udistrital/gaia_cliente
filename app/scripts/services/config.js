8ad589264e3eb7b86af66046739120e41a3c6170
Javier Reyes <ingjsreyesm@gmail.com>
2018-03-21 18:11:30 -0500

    Configurando variables de entorno para apuntar al APIMANAGER

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -45,16 +45,16 @@ var conf_cloud = {
         CLIENT_SECRET: "2crHq2IRkFHEVTBfpznLhKHyKVIa"
     },
 };
-var conf_presentacion = {
+var conf_preproduccion = {
     WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
     ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
-    ADMINISTRATIVA_MID_SERVICE: "http://10.20.0.210/administrativa_mid_api/v1/",
-    ADMINISTRATIVA_SERVICE: "http://10.20.0.210/administrativa_api/v1/",
-    ADMINISTRATIVA_PRUEBAS_SERVICE: "http://10.20.0.210/administrativa_amazon_api/v1/",
+    ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.udistrital.edu.co:8244/administrativa_mid_api/v1/",
+    ADMINISTRATIVA_SERVICE: "https://autenticacion.udistrital.edu.co:8244/administrativa_crud_api/v1/",
+    ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.udistrital.edu.co:8244/administrativa_amazon_api/v1/",
     ARKA_SERVICE: "http://10.20.0.210/arka_api_crud/v1/",
-    CONFIGURACION_SERVICE: "http://10.20.0.210/configuracion_api/v1/",
-    CORE_SERVICE: "http://10.20.0.210/core_api/v1/",
-    CORE_AMAZON_SERVICE: "http://10.20.0.210/core_amazon_crud/v1/",
+    CONFIGURACION_SERVICE: "https://autenticacion.udistrital.edu.co:8244/configuracion_crud_api/v1/",
+    CORE_SERVICE: "https://autenticacion.udistrital.edu.co:8244/core_api/v1/",
+    CORE_AMAZON_SERVICE: "https://autenticacion.udistrital.edu.co:8244/core_api/v1/",
     FINANCIERA_MID_SERVICE: "http://10.20.0.210/financiera_mid_api/v1/",
     FINANCIERA_SERVICE: "http://10.20.0.210/financiera_api/v1/",
     MODELS_SERVICE: "scripts/models/",
@@ -65,20 +65,21 @@ var conf_presentacion = {
     TITAN_SERVICE: "http://10.20.0.210/titan_api_crud/v1/",
     SICAPITAL_SERVICE: "http://10.20.0.127/sicws/ws/sicapitalAPI.php/?/",
     CONTRATO_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services/contratoSuscritoProxyService/",
+    NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
     HOMOLOGACION_SERVICE:"https://autenticacion.udistrital.edu.co:8244/dependencias_api/v1/",
     TOKEN: {
-        AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
-        URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
-        CLIENTE_ID: "XdBq4QOfEZYT0cl_8qDh3fmF5_Qa",
-        REDIRECT_URL: "http://localhost:9000/",
-        RESPONSE_TYPE: "code",
-        SCOPE: "openid email",
-        BUTTON_CLASS: "btn btn-warning btn-sm",
-        SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
-        SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
-        SIGN_OUT_APPEND_TOKEN: "true",
-        REFRESH_TOKEN: "https://autenticacion.udistrital.edu.co/oauth2/token",
-        CLIENT_SECRET: "lrVuDATX1o8TfXxz_jrEzBA2iIoa"
+      AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
+      URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
+      CLIENTE_ID: "OkoqitCdbVHCFdFfpEBjQHn0zVIa",
+      REDIRECT_URL: "https://pruebasadministrativa.portaloas.udistrital.edu.co/",
+      RESPONSE_TYPE: "code",
+      SCOPE: "openid email",
+      BUTTON_CLASS: "btn btn-warning btn-sm",
+      SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
+      SIGN_OUT_REDIRECT_URL: "https://pruebasadministrativa.portaloas.udistrital.edu.co/",
+      SIGN_OUT_APPEND_TOKEN: "true",
+      REFRESH_TOKEN: "https://autenticacion.udistrital.edu.co/oauth2/token",
+      CLIENT_SECRET: "w_288oYqoQ_Tz5DUrqRVNLkcV1Ea"
     },
 };
 
@@ -162,5 +163,5 @@ var conf_local = {
 
 angular.module('contractualClienteApp')
     .constant('CONF', {
-        GENERAL: conf_pruebas
+        GENERAL: conf_preproduccion
     });
