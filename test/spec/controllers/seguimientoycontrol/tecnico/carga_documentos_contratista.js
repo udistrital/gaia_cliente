c70a428310e2e4bc5fe335a260765d44528692f6
Oscar Duarte <o.duarte7@gmail.com>
2018-10-01 16:15:37 -0500

    Se usan mocks para simular servicios en pruebas unitarias

--- test/spec/controllers/seguimientoycontrol/tecnico/carga_documentos_contratista.js
+++ test/spec/controllers/seguimientoycontrol/tecnico/carga_documentos_contratista.js
@@ -5,27 +5,82 @@ describe('Controller: CargaDocumentosContratistaCtrl', function () {
   // load the controller's module
   beforeEach(module('contractualClienteApp'));
 
+  beforeEach(module(function ($provide) {
+    // inyectamos del mock
+      $provide.value('mockamazonAdministrativaRequest', {'get': function (tabla, params) {
+                return {
+                success: function (callback) {
+                  callback({
+                   // some fake response
+                   data:[{NomProveedor:"Pruebas"}]
+                  });
+                },
+                then: function(callback) {
+                   callback({
+                   // some fake response, you probably would want that to be
+                   // the same as for success 
+                   data:[{NomProveedor:"Pruebas"}]
+                   });
+                },
+                error: function(callback){
+                  callback({
+                   // some fake response
+                   data:null
+                  });             
+                }
+              }
+          }});
+  }));
+
+  beforeEach(module(function ($provide) {
+    // inyectamos del mock
+      $provide.value('mockadminMidRequest', {'get': function (tabla, params) {
+                return {
+                success: function (callback) {
+                  callback({
+                   // some fake response
+                   data:[{NumDocumentoSupervisor:123 }]
+                  });
+                },
+                then: function(callback) {
+                   callback({
+                   // some fake response, you probably would want that to be
+                   // the same as for success 
+                   data:[{NumDocumentoSupervisor:123 }]
+                   });
+                },
+                error: function(callback){
+                  callback({
+                   // some fake response
+                   data:null
+                  });             
+                }
+              }
+          }});
+  }));
+
   var cargaDocumentosContratistaCtrl,
     scope, $httpBackend, $adminMidRequest, contratos_contratista_handler, CONF ;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope,$injector) {
+  beforeEach(inject(function ($controller, $rootScope,$injector, mockamazonAdministrativaRequest, mockadminMidRequest) {
     scope = $rootScope.$new();
     $httpBackend = $injector.get('$httpBackend');
     $adminMidRequest=$injector.get('$httpBackend');
     CONF = $injector.get('CONF');
 
-    $httpBackend.when('GET',CONF.GENERAL.ADMINISTRATIVA_PRUEBAS_SERVICE+'informacion_proveedor/?query=NumDocumento%3A123&limit=0')
+    $httpBackend.when('GET','informacion_proveedor/?query=NumDocumento%3A123&limit=0')
     .respond({num:1});
 
     $httpBackend.when('GET',CONF.GENERAL.ADMINISTRATIVA_MID_SERVICE+'aprobacion_pago/contratos_contratista/123')
     .respond([{num:1}]);
 
     cargaDocumentosContratistaCtrl = $controller('cargaDocumentosContratistaCtrl', {
-      $scope: scope
+      $scope: scope,
+      amazonAdministrativaRequest: mockamazonAdministrativaRequest,
+      adminMidRequest: mockadminMidRequest
       // place here mocked dependencies
     });
-   // cargaDocumentosContratistaCtrl.Documento=1015438743;
 
   }));
 
@@ -44,33 +99,21 @@ describe('Controller: CargaDocumentosContratistaCtrl', function () {
 
   it('Debe obtener una lista con los 9 primeros meses del año', function () {
     cargaDocumentosContratistaCtrl.getMeses(2018);
-    expect(cargaDocumentosContratistaCtrl.meses.length).toBe(9);
+    expect(cargaDocumentosContratistaCtrl.meses.length).toBe(10);
   });
 
-  it('Debe obtener un arreglo de contratos del contratista', function () {
-    // cargaDocumentosContratistaCtrl.obtener_informacion_contratos_contratista();
-    // $adminMidRequest.expectGET('aprobacion_pago/contratos_contratista/123');
+  it('Debe obtener un arreglo de objetos con la información del contratista', function () {
   
     cargaDocumentosContratistaCtrl.obtener_informacion_contratista();
 
+    expect(cargaDocumentosContratistaCtrl.info_contratista).toEqual([{NomProveedor:"Pruebas"}]);
 
-
-   // $httpBackend.expectGET(CONF.GENERAL.ADMINISTRATIVA_PRUEBAS_SERVICE+'informacion_proveedor/?query=NumDocumento%3A123&limit=0');
-
-    //$httpBackend.flush();
-
-    expect(cargaDocumentosContratistaCtrl.info_contratista).toBe({num:1});
-
-
- //   $httpBackend.flush();
    });
 
   it('Debe obtener un arreglo de contratos del contratista', function () {
-   // cargaDocumentosContratistaCtrl.obtener_informacion_contratos_contratista();
-   // $adminMidRequest.expectGET('aprobacion_pago/contratos_contratista/123');
- 
-   $httpBackend.expectGET(CONF.GENERAL.ADMINISTRATIVA_MID_SERVICE+'/aprobacion_pago/contratos_contratista/123');
-       $httpBackend.flush();
+    cargaDocumentosContratistaCtrl.obtener_informacion_contratos_contratista();
+   expect(cargaDocumentosContratistaCtrl.informacion_contratos).toEqual([{NumDocumentoSupervisor:123 }]);
+
   });
 
 });
