4aec7d395de70f624242c863a093e0295cad6939
Brayanb24 <babermudezb@correo.udistrital.edu.co>
2019-08-03 18:13:37 -0500

    Corrigiendo path para preprod #11

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -171,23 +171,24 @@ var conf_preproduccion = {
 
 var conf_pruebas = {
   WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-  ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
+  // ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
   ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_mid_api/v1/",
   ADMINISTRATIVA_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_crud_api/v1/",
-  // ADMINISTRATIVA_MID_SERVICE: "http://10.20.0.254/administrativa_mid_api/v1/",
-  // ADMINISTRATIVA_SERVICE: "http://10.20.0.254/administrativa_api/v1/",
   ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_amazon_api/v1/",
   // CONFIGURACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/configuracion_crud_api/v1/",
   CONFIGURACION_SERVICE: "http://10.20.0.254/configuracion_api/v1/",
   CORE_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/core_api/v1/",
   CORE_AMAZON_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/core_amazon_crud/v1/",
+  // FINANCIERA_MID_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/financiera_mid_api/v1/",
   FINANCIERA_MID_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_mid_api/v1/",
-  FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/",
+  FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/", // financiara crud_api
+  // FINANCIERA_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/financiera_crud_api/v1/", // financiara crud_api
   MODELS_SERVICE: "scripts/models/",
-  OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
-  OIKOS_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_amazon_api/v1",
+  OIKOS_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/oikos_crud_api/v1/", //oikos_crud_api
+  // OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/", //oikos_crud_api
+  // OIKOS_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_amazon_api/v1", parece que no se usa
   RESOLUCION_SERVICE: "http://10.20.0.254/resoluciones_crud/v1/",
-  TITANDESAGREG_SERVICE : "http://api.intranetoas.udistrital.edu.co:8082/v1/",
+  TITANDESAGREG_SERVICE : "http://api.intranetoas.udistrital.edu.co:8082/v1/", // titan_api_mid
   TITAN_SERVICE: "https://tuleap.udistrital.edu.co/go_api/titan_api_crud/v1/",
   SICAPITAL_SERVICE: "http://10.20.0.127/sicws/ws/sicapitalAPI.php/?/",
   CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
@@ -253,7 +254,7 @@ var conf_pruebas = {
 
 var conf_local = {
   WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-  ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
+  // ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
   ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_mid_api/v1/",
   ADMINISTRATIVA_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_crud_api/v1/",
   // ADMINISTRATIVA_MID_SERVICE: "http://10.20.0.254/administrativa_mid_api/v1/",
@@ -263,13 +264,16 @@ var conf_local = {
   CONFIGURACION_SERVICE: "http://10.20.0.254/configuracion_api/v1/",
   CORE_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/core_api/v1/",
   CORE_AMAZON_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/core_amazon_crud/v1/",
+  // FINANCIERA_MID_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/financiera_mid_api/v1/",
   FINANCIERA_MID_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_mid_api/v1/",
-  FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/",
+  FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/", // financiara crud_api
+  // FINANCIERA_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/financiera_crud_api/v1/", // financiara crud_api
   MODELS_SERVICE: "scripts/models/",
-  OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
-  OIKOS_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_amazon_api/v1",
+  OIKOS_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/oikos_crud_api/v1/", //oikos_crud_api
+  // OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/", //oikos_crud_api
+  // OIKOS_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_amazon_api/v1", parece que no se usa
   RESOLUCION_SERVICE: "http://10.20.0.254/resoluciones_crud/v1/",
-  TITANDESAGREG_SERVICE : "http://api.intranetoas.udistrital.edu.co:8082/v1/",
+  TITANDESAGREG_SERVICE : "http://api.intranetoas.udistrital.edu.co:8082/v1/", // titan_api_mid
   TITAN_SERVICE: "https://tuleap.udistrital.edu.co/go_api/titan_api_crud/v1/",
   SICAPITAL_SERVICE: "http://10.20.0.127/sicws/ws/sicapitalAPI.php/?/",
   CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
@@ -423,5 +427,5 @@ var conf_local = {
 
 angular.module('contractualClienteApp')
     .constant('CONF', {
-        GENERAL: conf_resoluciones,
+        GENERAL: conf_local,
     });
