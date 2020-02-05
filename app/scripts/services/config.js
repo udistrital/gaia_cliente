7198aad5048d560c331c8ab89ad8ba09284b4161
Brayanb24 <babermudezb@correo.udistrital.edu.co>
2020-02-05 10:27:39 -0500

    preparando servicios para test #57

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -249,6 +249,89 @@ var conf_local = {
     ]
 };
 
+var conf_resoluciones_preprod = {
+    WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
+    // ACADEMICA_SERVICE: "http://10.20.0.127/urano/index.php?data=B-7djBQWvIdLAEEycbH1n6e-3dACi5eLUOb63vMYhGq0kPBs7NGLYWFCL0RSTCu1yTlE5hH854MOgmjuVfPWyvdpaJDUOyByX-ksEPFIrrQQ7t1p4BkZcBuGD2cgJXeD",
+    CONFIGURACION_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8086/v1/",
+    //Servicio apuntando a servidores de la oficina 
+    ADMINISTRATIVA_MID_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8091/v1/",
+    //ADMINISTRATIVA_MID_SERVICE: "http://localhost:8091/v1/",
+    ADMINISTRATIVA_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8090/v1/",
+    CORE_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8092/v1/",
+    CORE_AMAZON_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8106/v1/",
+    // RESOLUCION_SERVICE: "http://10.20.0.254/resoluciones_crud/v1/",
+    ADMINISTRATIVA_PRUEBAS_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8104/v1/",
+    FINANCIERA_MID_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8089/v1/",
+    FINANCIERA_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8084/v1/",
+    MODELS_SERVICE: "scripts/models/",
+    OIKOS_SERVICE: "http://pruebasapi.intranetoas.udistrital.edu.co:8087/v2/",
+    //OIKOS_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_amazon_api/v1",
+    //TITAN_SERVICE: "https://tuleap.udistrital.edu.co/go_api/titan_api_crud/v1/",
+    //TITANDESAGREG_SERVICE : "https://tuleap.udistrital.edu.co/go_api/titan_api_crud/v1/",
+    TITANDESAGREG_SERVICE: "http://api.intranetoas.udistrital.edu.co:8082/v1/",
+    //SICAPITAL_SERVICE: "http://10.20.0.127/sicws/ws/sicapitalAPI.php/?/",
+    CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
+    NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
+    HOMOLOGACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/dependencias_api/v1/",
+    TOKEN: {
+        AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
+        URL_USER_INFO: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
+        CLIENTE_ID: "e36v1MPQk2jbz9KM4SmKhk8Cyw0a",
+        REDIRECT_URL: "http://localhost:9000/",
+        RESPONSE_TYPE: "id_token token",
+        SCOPE: "openid email documento",
+        BUTTON_CLASS: "btn btn-warning btn-sm",
+        SIGN_OUT_URL: "http://localhost:9000/",
+        SIGN_OUT_REDIRECT_URL: "https://pruebasadministrativa.portaloas.udistrital.edu.co",
+        SIGN_OUT_APPEND_TOKEN: "true",
+    },
+    MENU_APP: [{
+            id: "kronos",
+            title: "KRONOS",
+            url: "http://10.20.0.254/kronos"
+        }, {
+            id: "agora",
+            title: "AGORA",
+            url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/agora"
+        },
+        {
+            id: "argo",
+            title: "ARGO",
+            url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/argo"
+        },
+        {
+            id: "arka",
+            title: "ARKA",
+            url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/arka"
+        },
+        {
+            id: "temis",
+            title: "TEMIS",
+            url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/gefad"
+        },
+        {
+            id: "polux",
+            title: "POLUX",
+            url: "http://10.20.0.254/polux"
+        },
+        {
+            id: "jano",
+            title: "JANO",
+            url: "http://10.20.0.254/kronos"
+        },
+        {
+            id: "kyron",
+            title: "KYRON",
+            url: "http://10.20.0.254/kronos"
+        },
+        {
+            id: "sga",
+            title: "SGA",
+            url: "http://10.20.0.254/kronos"
+        }
+    ]
+};
+
 
 var conf_resoluciones = {
     WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
@@ -335,5 +418,5 @@ var conf_resoluciones = {
 
 angular.module('contractualClienteApp')
     .constant('CONF', {
-        GENERAL: conf_resoluciones,
+        GENERAL: conf_resoluciones_preprod,
     });
