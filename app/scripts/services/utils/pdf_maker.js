34109cfeb694a0b9faf97ca32153290c4940001f
jupmorenor <jupmorenor@gmail.com>
2022-02-04 12:09:29 -0500

    hotfix: Se corrige fallo que impedía visualizar resoluciones que no han sido expedidas #80

--- app/scripts/services/utils/pdf_maker.js
+++ app/scripts/services/utils/pdf_maker.js
@@ -716,7 +716,7 @@ angular.module('resolucionesClienteApp')
 
         //Obtener tabla del final
         self.getTablaRevision = function (contenido) {
-            if(contenido.CuadroResponsabilidades !== undefined){
+            if(contenido.CuadroResponsabilidades.length > 0){
                 var cuadroResponsabilidades = JSON.parse(contenido.CuadroResponsabilidades);
                 var columns = cuadroResponsabilidades.columns.map(function (columna) {
                     return { text: columna.value, style: "tabla_revision" };
@@ -751,47 +751,39 @@ angular.module('resolucionesClienteApp')
                         }
                 };
             } else {
-                return {
-                    style: 'tabla_revision',
+                if (contenido.Vinculacion.NivelAcademico === "PREGRADO") {
+                    return {
+                        style: 'tabla_revision',
                         table: {
                             headerRows: 1,
                             widths: [80, 150, 150, 80],
-                            body: []
+                            body: [
+                                ['', { text: $translate.instant('NOMBRE'), style: 'tabla_revision' }, { text: $translate.instant('CARGO_PDF'), style: 'tabla_revision' }, { text: $translate.instant('FIRMA'), style: 'tabla_revision' }],
+                                [{ text: $translate.instant('ELABORO'), style: 'tabla_revision' }, { text: 'Grupo de trabajo contratación docente facultades e ILUD', style: 'tabla_revision' }, { text: 'Funcionarios y Contratistas', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Javier Bolaños Zambrano', style: 'tabla_revision' }, { text: 'Jefe Oficina Asesora Jurídica', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Adriana Marcela Sandoval Castiblanco', style: 'tabla_revision' }, { text: 'Secretaria General', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'William Fernando Castrillón Cardona', style: 'tabla_revision' }, { text: 'Vicerrector Académico', style: 'tabla_revision' }, ''],
+                            ]
                         }
-                };
+                    };
+                } else {
+                    return {
+                        style: 'tabla_revision',
+                        table: {
+                            headerRows: 1,
+                            widths: [80, 150, 150, 80],
+                            body: [
+                                ['', { text: $translate.instant('NOMBRE_COMPLETO'), style: 'tabla_revision' }, { text: $translate.instant('CARGO_PDF'), style: 'tabla_revision' }, { text: $translate.instant('FIRMA'), style: 'tabla_revision' }],
+                                [{ text: $translate.instant('ELABORO'), style: 'tabla_revision' }, { text: 'Grupo de trabajo contratación docente facultades e ILUD', style: 'tabla_revision' }, { text: 'Funcionarios y Contratistas', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_AJUSTO'), style: 'tabla_revision' }, { text: 'Diana Ximena Pirachicán Martinez', style: 'tabla_revision' }, { text: 'Asesor CPS OAJ', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO2'), style: 'tabla_revision' }, { text: 'Milena Isabel Rubiano Rojas', style: 'tabla_revision' }, { text: 'Jefe Oficina Asesora Jurídica(E)', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Adriana Marcela Sandoval Castiblanco', style: 'tabla_revision' }, { text: 'Secretaria General', style: 'tabla_revision' }, ''],
+                                [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'William Fernando Castrillón Cardona', style: 'tabla_revision' }, { text: 'Vicerrector Académico', style: 'tabla_revision' }, ''],
+                            ]
+                        }
+                    };
+                }
             }
-            /* if(resolucionNivelAcademico === "PREGRADO") {
-                return {
-                    style: 'tabla_revision',
-                    table: {
-                        headerRows: 1,
-                        widths: [80, 150, 150, 80],
-                        body: [
-                            ['', { text: $translate.instant('NOMBRE'), style: 'tabla_revision' }, { text: $translate.instant('CARGO_PDF'), style: 'tabla_revision' }, { text: $translate.instant('FIRMA'), style: 'tabla_revision' }],
-                            [{ text: $translate.instant('ELABORO'), style: 'tabla_revision' }, { text: 'Grupo de trabajo contratación docente facultades e ILUD', style: 'tabla_revision' }, { text: 'Funcionarios y Contratistas', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Javier Bolaños Zambrano', style: 'tabla_revision' }, { text: 'Jefe Oficina Asesora Jurídica', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Adriana Marcela Sandoval Castiblanco', style: 'tabla_revision' }, { text: 'Secretaria General', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'William Fernando Castrillón Cardona', style: 'tabla_revision' }, { text: 'Vicerrector Académico', style: 'tabla_revision' }, ''],
-                        ]
-                    }
-                };
-            } else {
-                return {
-                    style: 'tabla_revision',
-                    table: {
-                        headerRows: 1,
-                        widths: [80, 150, 150, 80],
-                        body: [
-                            ['', { text: $translate.instant('NOMBRE_COMPLETO'), style: 'tabla_revision' }, { text: $translate.instant('CARGO_PDF'), style: 'tabla_revision' }, { text: $translate.instant('FIRMA'), style: 'tabla_revision' }],
-                            [{ text: $translate.instant('ELABORO'), style: 'tabla_revision' }, { text: 'Grupo de trabajo contratación docente facultades e ILUD', style: 'tabla_revision' }, { text: 'Funcionarios y Contratistas', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_AJUSTO'), style: 'tabla_revision' }, { text: 'Diana Ximena Pirachicán Martinez', style: 'tabla_revision' }, { text: 'Asesor CPS OAJ', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO2'), style: 'tabla_revision' }, { text: 'Milena Isabel Rubiano Rojas', style: 'tabla_revision' }, { text: 'Jefe Oficina Asesora Jurídica(E)', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'Adriana Marcela Sandoval Castiblanco', style: 'tabla_revision' }, { text: 'Secretaria General', style: 'tabla_revision' }, ''],
-                            [{ text: $translate.instant('REVISO_APROBO'), style: 'tabla_revision' }, { text: 'William Fernando Castrillón Cardona', style: 'tabla_revision' }, { text: 'Vicerrector Académico', style: 'tabla_revision' }, ''],
-                        ]
-                    }
-                };
-            } */
         };
 
         //Función para obtener el texto del preámbulo dentro de una estructura
